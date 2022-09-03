8e15dbfba45991fbf1f03a8dfcfd9bc7c8e34209
Debarati Das <debarati84430@gmail.com>
2022-09-03 22:55:04 +0530

    change done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { Container } from "react-bootstrap";
 import AreaOfCircle from "./Component/AreaOfCircle";
 import AreaOfReactangle from "./Component/AreaOfReactangle";
+import Header from "./Component/Header";
+import Grid from "./Component/Grid";
+import "./App.css";
 function App() {
   return (
-    <div>
-      <AreaOfReactangle />
-      <AreaOfCircle />
+    <div className="amarContainer">
+      <Header />
+      {/* <Row>
+        <Col></Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row> */}
+      <Grid />
+      <Container>
+        <AreaOfReactangle />
+        <AreaOfCircle />
+      </Container>
     </div>
   );
 }
