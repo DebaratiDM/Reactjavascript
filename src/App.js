7ac4c36e650057db4986af2bf6d3d74bc7a3a16c
Debarati Das <debarati84430@gmail.com>
2022-09-03 17:03:17 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import AreaOfCircle from "./Component/AreaOfCircle";
 import AreaOfReactangle from "./Component/AreaOfReactangle";
 function App() {
-  return <AreaOfReactangle />;
+  return (
+    <div>
+      <AreaOfReactangle />
+      <AreaOfCircle />
+    </div>
+  );
 }
 
 export default App;
